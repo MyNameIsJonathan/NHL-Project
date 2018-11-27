d0d4cb17b59cf51799c1407189425554d8694355
Jonathan <jonathanholson@gmail.com>
2018-11-27 11:13:11 -0800

    Created NHL Stats Page
    Removed "About" page and instead included NHL Stats

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -8,12 +8,16 @@ main = Blueprint('main', __name__)
 
 #These functions 'create' pages within your site. 
 # This one creates the 'home' page, while the one 
-# below creates the About page
+# below creates the NHL Stats page
 @main.route("/")  #Having two @app. here means that this page is rendered when either website.com/ or website.com/home are visited
 @main.route("/home")
 def home():
     page = request.args.get('page', default=1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts)
+
+@main.route("/nhl_stats")
+def nhl_stats():
     a = pd.read_pickle('/Users/jonathanolson/Documents/GitHub/NHL-Project/dailyMyDF/dailyMyDF_2018-11-25.pickle')
     a = a.sort_values('G', ascending=False)
     my_results = {}
@@ -22,11 +26,7 @@ def home():
     my_results['Third Top Scorer'] = a.iloc[2].name
     my_results['Fourth Top Scorer'] = a.iloc[3].name
     my_results['Fifth Top Scorer'] = a.iloc[4].name
-    return render_template('home.html', posts=posts, my_results=my_results)
-
-@main.route("/about")
-def about():
-    return render_template('about.html', title = 'About')
+    return render_template('nhl_stats.html', title = 'NHL Stats', my_results=my_results)
 
 @main.route("/announcements")
 def announcements():
--- flaskrun.py
+++ flaskrun.py
@@ -2,7 +2,5 @@ from flaskblog import create_app
 
 app = create_app()
 
-a = 'Steven Stamkos'
-
 if __name__ == '__main__':
     app.run(debug=True)
