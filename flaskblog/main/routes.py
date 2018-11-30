32d2c682b5b5d3fd870752c7faa160ece02bd034
Jonathan <jonathanholson@gmail.com>
2018-11-30 12:59:22 -0800

    Cool Style Updates
    Updated page layout

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -2,6 +2,9 @@ from flask import render_template, request, Blueprint
 from flaskblog.models import Post
 import pandas as pd
 import NHL_scrape_functions
+import datetime
+
+# source /Users/jonathanolson/Projects/Environments/nhl_flask/bin/activate
 
 main = Blueprint('main', __name__)
 
@@ -12,53 +15,40 @@ main = Blueprint('main', __name__)
 @main.route("/")  #Having two @app. here means that this page is rendered when either website.com/ or website.com/home are visited
 @main.route("/home")
 def home():
-    page = request.args.get('page', default=1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 @main.route("/nhl_stats")
 def nhl_stats():
+
+    # Load in the three main dataframes and select for current players, if necessary
     myDF = NHL_scrape_functions.openLatestMyDF().sort_values('G', ascending=False)
+    myDF['TOI'] = myDF['TOI'].astype(int)
+    myDF = myDF.sort_values(['G', 'PTS'], ascending=False)
+    myDF = myDF.reindex(['G', 'A', 'PTS', '+/-', 'PIM', 'EV', 'PP', 'SH', 'GW', 'S', 'Shifts', 'TOI'], axis=1)
+
     lastTimeDF = NHL_scrape_functions.openLatestLastTime()
+    lastTimeDF = lastTimeDF[lastTimeDF['Last Game Date'] >= pd.to_datetime('today').date() - datetime.timedelta(days=30)] # Select for player who've played in last 30 days only
+    lastTimeDF = lastTimeDF[lastTimeDF['G'] > pd.to_datetime('2000-10-03', format="%Y-%m-%d").date()] # Players who've never scored have last goal set as 2000/10/3. Remove these palyers
+    lastTimeDF = lastTimeDF.sort_values(['G', 'A'])
+    lastTimeDF = lastTimeDF.reindex(['Last Game Date', 'G', 'A', 'PTS', '+', '-', 'PIM', 'EV', 'PP', 'SH', 'GW', 'S'], axis=1)
+
+    # Grab the players in this df, for slicing retired players from the gamesSince DF
+    currentPlayers = lastTimeDF.index
+
     gamesSinceDF = NHL_scrape_functions.openLatestGamesSince()
+    gamesSinceDF = gamesSinceDF.loc[currentPlayers, :]
+    gamesSinceDF = gamesSinceDF.sort_values(['G', 'A'], ascending=False)
 
-    #Create a dictionary to store important pieces of info, to be displayed on NHL_stats page
-    importantStats = {}
-
-    #Get top goal scorers -- if tied, get top point scorer.
-    importantStats['Top Scorer'] = myDF.sort_values(['G', 'PTS'], ascending=False).iloc[0].name
-    importantStats['Second Top Scorer'] = myDF.sort_values(['G', 'PTS'], ascending=False).iloc[1].name
-    importantStats['Third Top Scorer'] = myDF.sort_values(['G', 'PTS'], ascending=False).iloc[2].name
-    importantStats['Fourth Top Scorer'] = myDF.sort_values(['G', 'PTS'], ascending=False).iloc[3].name
-    importantStats['Fifth Top Scorer'] = myDF.sort_values(['G', 'PTS'], ascending=False).iloc[4].name
+    # Convert DFs to html
+    myDF=myDF.head(10).to_html(classes=['table', 'table-striped', 'table-hover', 'stat-table'], index_names=False, justify='center')
+    lastTimeDF=lastTimeDF.head(10).to_html(classes=['table', 'table-striped', 'table-hover', 'stat-table'], index_names=False, table_id="lasttime-table", justify='center')
+    gamesSinceDF=gamesSinceDF.head(10).to_html(classes=['table', 'table-striped', 'table-hover', 'stat-table'], index_names=False, justify='center')
 
     # Set pandas options
     pd.set_option('colheader_justify', 'center') 
 
-    my_html_df = myDF.head(10).to_html()
+    return render_template('nhl_stats.html', title = 'NHL Stats', myDF=myDF, lastTimeDF=lastTimeDF, gamesSinceDF=gamesSinceDF)
 
-    # Initialize an html string to fill in the table/df
-    html_string = f'''
-        <html>
-        <head><title>Current NHL Leaders</title></head>
-        <link rel="stylesheet" type="text/css" href="df_style.css"/>
-        <body>
-            {my_html_df}
-        </body>
-        </html>.
-        '''
-
-    # Write an html file with the html version of this dataframe
-    with open('MyDF_html.html', 'w') as f:
-        f.write(html_string)
-        f.close()
-
-    print('htmlstring:', html_string)
-
-    return render_template('nhl_stats.html', 
-    title = 'NHL Stats', 
-    importantStats=importantStats, 
-    myDF=myDF.head(10).to_html(classes=['table-striped', 'table-hover', 'table-sm', 'table-responsive', 'table-borderless', 'stat-table'], index_names=False))
 
 @main.route("/announcements")
 def announcements():
