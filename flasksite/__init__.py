36165261aabd4b8d104c2b991bd264347b5b5489
Jonathan <jonathanholson@gmail.com>
2019-02-14 16:34:41 -0800

    Attempted implementation of flask_mysqldb

--- flasksite/__init__.py
+++ flasksite/__init__.py
@@ -4,13 +4,17 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flasksite.config import Config
 
-
+# Initialize database for user login
 db = SQLAlchemy()
+
+# Initialize database for stats
+mysql = MySQL()
 
 #Initialize our method for encrypting our submitted passwords
 bcrypt = Bcrypt() 
@@ -26,18 +30,18 @@ login_manager.login_message_category = 'info' #Makes the alerts for required log
 mail = Mail()
 
 
-
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    mysql.init_app(app)
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
 
     #Import down here to prevent circular imports
-    from flasksite.users.routes import users #Here, we are importing the "users" Blutprint variable
+    from flasksite.users.routes import users #Here, we are importing the "users" Blueprint variable
     from flasksite.main.routes import main
     from flasksite.errors.handlers import errors
     app.register_blueprint(users)
--- flasksite/main/routes.py
+++ flasksite/main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, Blueprint, send_from_directory
-from flasksite.config import Config
+from flasksite import mysql
 import NHL_scrape_functions as nhl
 import tweepyrun as twp
 import pandas as pd
@@ -8,9 +8,9 @@ import random
 import os
 import ast
 
+
 # Create a blueprint for the Flask site
 main = Blueprint('main', __name__)
-engine = nhl.createEngine()
 
 # Instantiate the route decorators for Flask
 @main.route("/")
@@ -21,8 +21,28 @@ def home():
 @main.route("/nhl_stats")
 def nhl_stats():
 
+    # THIS IS OPENTODAYSHTML
+
     # Open dict of today's HTML files
-    todaysHTML = nhl.opentodaysHTML(engine)
+    cursor = mysql.get_db().cursor()
+
+    # Get the last row of the table, sorted by date. This means itll be today's html, unless there was an error, allowing it to fall back on yesterdays html
+    myHTML = cursor.execute(f"SELECT * FROM dailyDataFrames ORDER BY date DESC LIMIT 1").fetchone()
+
+    # Select each component from myHTML (index: value --> 0: id, 1: date, 2: mydf, 3: lastTime, 4: gamesSince). Convert these dicts to DataFrames
+    mydf = pd.DataFrame.from_dict(json.loads(myHTML['stats']))
+    lastTime = pd.DataFrame.from_dict(json.loads(myHTML['lastTime']))
+    gamesSince = pd.DataFrame.from_dict(json.loads(myHTML['gamesSince']))
+
+    # Convert DFs to html
+    myDFHTML = mydf.head(10).to_html(classes=['table', 'stat-table'], index_names=False, justify='center')
+    lastTimeHTML = lastTime.head(10).to_html(classes=['table', 'stat-table'], index_names=False, justify='center')
+    gamesSinceHTML = gamesSince.head(10).to_html(classes=['table', 'stat-table'], index_names=False, justify='center')
+
+    todaysHTML = {
+        'myDF': myDFHTML,
+        'lastTime': lastTimeHTML,
+        'gamesSince': gamesSinceHTML}
 
     return render_template('nhl_stats.html', title = 'NHL Stats', myDF=todaysHTML['myDF'], lastTimeDF=todaysHTML['lastTimeDF'], gamesSinceDF=todaysHTML['gamesSinceDF'])
 
@@ -30,7 +50,10 @@ def nhl_stats():
 def todays_players():
 
     # Open dict of todays drought leaders and int of number of players today
-    todaysDroughts = nhl.openTodaysDroughts(engine)
+    today = pd.to_datetime('today').date()
+    cursor = mysql.get_db().cursor()
+    todaysDroughts = cursor.execute(f"SELECT * FROM todaysDroughts WHERE Date = '{today}'").fetchone()
+
     droughtsDict = ast.literal_eval(todaysDroughts[2])
     numberOfPlayersToday = todaysDroughts[-1]
 
@@ -39,8 +62,10 @@ def todays_players():
 @main.route("/stamkostweets")
 def stamkostweets():
 
+    cursor = mysql.get_db().cursor()
+
     #Open tweets mentioning stamkos from the last week
-    my_tweets = nhl.openMySQLTable('stamkosTweets', engine)
+    my_tweets = cursor.execute(f"SELECT * FROM stamkosTweets ORDER BY date DESC LIMIT 150").fetchone()
 
     # Choose a random order of tweets
     # my_numbers = random.sample(range(len(my_tweets)), len(my_tweets))
