40b1c7d46d9ae32841e909a70f7599206682d184
Jonathan <jonathanholson@gmail.com>
2019-02-27 15:44:10 -0800

    Minor update to attempt to fix sqlalchemy communications confusion

--- scraperun.py
+++ scraperun.py
@@ -1,6 +1,5 @@
 import NHL_scrape_functions as nhl
 
 if __name__ == '__main__':
-    engine = nhl.nonFlaskCreateEngine()
-    # backupsEngine = nhl.createEngine(database='backups')
-    nhl.scrapeToToday(engine)
+    with nhl.nonFlaskCreateEngine() as engine:
+        nhl.scrapeToToday(engine)
