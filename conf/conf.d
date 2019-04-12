43354d777ca9732cd992f1eb3adc2de902992a87
Jonathan Olson <jonathanholson@gmail.com>
2019-04-11 20:09:01 -0700

    Mostly working -- site can't find CSS

--- conf/conf.d
+++ conf/conf.d
@@ -1,14 +1,27 @@
-server {
-    listen 80;
-    server_name localhost;
+events { }
 
-    location /static {
-        alias /NHL-Project/flasksite/static;
+http {
+
+    upstream upstream-web {
+        server jonathanolson.us;
     }
 
-    location / {
-        proxy_pass http:/localhost:8000;
-        include /etc/nginx/proxy_params;
-        proxy_redirect off;
+    server {
+        listen 80;
+        server_name gunicornservice;
+
+        location /static {
+            alias /NHL-Project/flasksite/static;
+        }
+
+        location / {
+            proxy_pass http://gunicornservice:8000;
+            # include /etc/nginx/proxy_params;
+            proxy_redirect off;
+            proxy_set_header   Host $host;
+            proxy_set_header   X-Real-IP $remote_addr;
+            proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header   X-Forwarded-Host $server_name;
+        }
     }
 }
