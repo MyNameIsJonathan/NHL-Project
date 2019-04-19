d652ab5fda0be137cb8eeefd22c5e1d706f77031
Jonathan <jonathanholson@gmail.com>
2019-04-19 16:01:45 -0700

    Formatted tweepyrun.py for docker

--- flasksite/config.py
+++ flasksite/config.py
@@ -14,3 +14,9 @@ class Config():
     MYSQL_DATABASE_PORT = config.get("MYSQL_PORT")
     MYSQL_DATABASE_HOST = config.get("MYSQL_HOST")
     MYSQL_DATABASE_USER = config.get("MYSQL_USER")
+
+    # For Tweepy
+    my_consumer_key = config.get("my_consumer_key")
+    my_consumer_secret = config.get("my_consumer_secret")
+    my_access_token = config.get("my_access_token")
+    my_access_token_secret = config.get("my_access_token_secret")
