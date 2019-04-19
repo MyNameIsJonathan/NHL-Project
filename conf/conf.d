b65af88c9d959285dd6d854aa231160d2deac292
Jonathan Olson <jonathanholson@gmail.com>
2019-04-19 15:32:38 -0700

    Fully functional site via docker

--- conf/conf.d
+++ conf/conf.d
@@ -19,5 +19,23 @@ http {
             proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header   X-Forwarded-Host $server_name;
         }
+
+        listen 443 ssl; # managed by Certbot
+        ssl_certificate /etc/letsencrypt/live/jonathanolson.us/fullchain.pem; # managed by Certbot
+        ssl_certificate_key /etc/letsencrypt/live/jonathanolson.us/privkey.pem; # managed by Certbot
+        include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+        ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+
+    }
+
+    server {
+        if ($host = jonathanolson.us) {
+            return 301 https://$host$request_uri;
+        } # managed by Certbot
+
+
+        listen 80;
+        server_name jonathanolson.us;
+        return 404; # managed by Certbot
     }
 }
