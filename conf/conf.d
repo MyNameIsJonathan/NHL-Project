b4a2df6ef766a35187a623e524ae16b5ba9de495
Jonathan Olson <jonathanholson@gmail.com>
2019-05-08 20:46:04 -0700

    Fixed Sign in, Redirect issues. Fully functional

--- conf/conf.d
+++ conf/conf.d
@@ -8,17 +8,22 @@ http {
 
     server {
         listen 80;
-        server_name gunicornservice;
+        server_name jonathanolson.us;
 
         location / {
 	    root /NHL-Project/flasksite/static;
             proxy_pass http://gunicornservice:8000;
             proxy_redirect off;
-            proxy_set_header   Host $host;
-            proxy_set_header   X-Real-IP $remote_addr;
-            proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header   X-Forwarded-Host $server_name;
+            proxy_set_header   Host                 $host;
+            proxy_set_header   X-Real-IP            $remote_addr;
+            proxy_set_header   X-Forwarded-For      $proxy_add_x_forwarded_for;
+            proxy_set_header   X-Forwarded-Host     $host:443;
+            proxy_set_header   X-Forwarded-Server   $host;
+            proxy_set_header   X-Forwarded-Port     443;
+            proxy_set_header   X-Forwarded-Proto    https;
         }
+
+        error_page 497 https://$host:$server_port$request_uri;
 
         listen 443 ssl; # managed by Certbot
         ssl_certificate /etc/letsencrypt/live/jonathanolson.us/fullchain.pem; # managed by Certbot
